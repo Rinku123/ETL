5e0d929ee00dac59f01d09fe507aa9fe2fd691cf
rinku <acumenrinku@gmail.com>
2015-01-25 01:44:27 -0500

    Initial check for ETL

---
+++ etl/src/main/java/org/hadoop/etl/etl/App.java
@@ -0,0 +1,13 @@
+package org.hadoop.etl.etl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
